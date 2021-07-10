59b42e5305a0d464b17f82ddea36e3c9ab95cf1b
Sugar Shane <scrisostomo17@gmail.com>
2021-07-10 12:08:22 -0400

    completed 5 day forecast

--- asset/js/script.js
+++ asset/js/script.js
@@ -29,7 +29,7 @@ function getWeather(cityName){
 
         cityNameElement.innerHTML = nameValue;
         currentDate.innerHTML = currentHour;
-        currentTempElement.innerHTML = "Temp: " + tempValue + 'F';
+        currentTempElement.innerHTML = "Temperature: " + tempValue + 'F';
         currentWindElement.innerHTML = "Wind Speed: " + windValue;
         currentHumidity.innerHTML = "Humidity: " + humidValue + "%";
        
@@ -51,25 +51,55 @@ function getWeather(cityName){
 
        
         var forecastData = data.daily;
+        // console.log(forecastData[0].temp.day)
 
         for (let index = 0; index < forecastData.length; index++) {
             const element = forecastData[index];
-            console.log(forecastData[index]);
+            var temp = Math.round(((parseFloat(element.temp.day)-273.15)*1.8)+32) + '&deg' + 'F';
+            console.log(element);
+            // var currentHour = moment.unix(data.dt).format('MMMM Do YYYY')
 
-            // document.getElementById(`pic-${index}`).innerHTML = temp.day;
-            document.getElementById(`temperature-${index}`).innerHTML = temp.day;
-            document.getElementById(`wind-speed-${index}`).innerHTML = data.temp.day;
-            document.getElementById(`humidity-${index}`).innerHTML = data.humidity;
-            document.getElementById(`UV-index-${index}`).innerHTML = data.uvi;
 
-                
-            console.log(document.getElementById(`temperature-${index}`));
+            // document.getElementById(`pic-${index}`).innerHTML = element.temp.day;
+            document.getElementById(`temperature-${index}`).innerHTML = "Temperature: " + temp;
+            document.getElementById(`wind-speed-${index}`).innerHTML = "Wind: " + element.wind_speed;
+            document.getElementById(`humidity-${index}`).innerHTML = "Humidity: " + element.humidity;
+            document.getElementById(`UV-index-${index}`).innerHTML = "UV-Index: " + element.uvi;
+
+        
         }
 
      })
 
     })
 }
+
+var saveSearch = document.getElementById("search-button");
+saveSearch.onclick = saveData;
+
+function saveData(){
+  var input = document.getElementById("enter-city");
+  localStorage.setItem("city", input.value);
+  var storedValue = localStorage.getItem("city");
+
+  var newCity = document.createElement("button");
+  newCity.setAttribute('id', 'newCity');
+  newCity.innerHTML = storedValue; 
+
+  document.getElementById("cities").appendChild(newCity);
+}
+
+var history = document.getElementById("newCity");
+
+
+function citySearch () {
+    document.getElementById("newCity");
+    
+}
+
+history.onclick = citySearch();
+
+
 
 //add event listener when the button is clicked
     searchElement.addEventListener('click', function(){
